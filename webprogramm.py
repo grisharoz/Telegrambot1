e526dd8556ae5b76facdc886cabc2d705f119064
Grigorii Rozentul <162689186+grisharoz@users.noreply.github.com>
2024-08-22 10:44:01 +0300

    it will change
    Веб приложение начало

---
+++ webprogramm.py
@@ -0,0 +1,10 @@
+from aiogram import Bot,Dispatcher,executor,types
+from aiogram.types.web_app_info import WebAppInfo
+bot=Bot('7512813136:AAHMaQ5hz9K_8FeQiwT-_zgIJbYtnWVElx8')
+dp=Dispatcher(bot)
+@dp.message_handler(commands=['start'])
+async def start(message):
+     markup=types.ReplyKeyboardMarkup()
+     markup.add(types.KeyboardButton('Открыть веб страницу',web_app=WebAppInfo))
+
+executor.start_polling(dp)
